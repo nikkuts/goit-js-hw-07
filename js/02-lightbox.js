527fe5d91c8f72183a89dbe5008d191d80329b46
nikkuts <nickkuts888@gmail.com>
2023-02-25 15:43:21 +0200

    завдання 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,32 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryRef = document.querySelector(".gallery");
+const markup = createMarkup(galleryItems);
+galleryRef.innerHTML = markup;
+
+function createMarkup(items) {
+    return items
+    .map(({preview, original, description}) => 
+    `<a class="gallery__item" href="${original}">
+    <img class="gallery__image" 
+    src="${preview}" alt="${description}" 
+    />
+    </a>`)
+    .join("");
+}
+
+galleryRef.addEventListener("click", onShowsImage);
+
+function onShowsImage(event) {
+    event.preventDefault();
+    if (event.target.nodeName !== 'IMG') {
+        return;
+    } 
+    var lightbox = new SimpleLightbox('.gallery a', { 
+        captionsData: 'alt',
+        captionPosition: 'bottom',
+        captionDelay: 250, 
+    });
+}
